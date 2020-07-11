e7e1621da381e1f81225536f07adec7b850bb154
keith <khr877@gmail.com>
2020-07-10 21:14:44 -0700

    Added title screen to game

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    public void SinglePlayer(){
+    	// When AI is implemented
+    }
+    public void MultiPlayer(){
+    	SceneManager.LoadScene("Game");
+    }
+}
